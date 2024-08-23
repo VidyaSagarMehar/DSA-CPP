40b64a74b6b6f1acd4c2a2c4e4917a4d435a8310
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-08-23 12:21:54 +0530

    added in pattern printing

--- Pattern Printing/binaryTriangle.cpp
+++ Pattern Printing/binaryTriangle.cpp
@@ -6,20 +6,34 @@ int main()
     cout << "Enter the no. of rows : ";
     cin >> n;
     int a = 5;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if (i % 2 != 0)
+    //         a = 1; // row no. odd
+    //     else
+    //         a = 0; // row no. even
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << a;
+    //         // flipping
+    //         if (a == 1)
+    //             a = 0;
+    //         else
+    //             a = 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // OR METHOD 2
+
     for (int i = 1; i <= n; i++)
     {
-        if (i % 2 != 0)
-            a = 1; // row no. odd
-        else
-            a = 0; // row no. even
         for (int j = 1; j <= i; j++)
         {
-            cout << a;
-            // flipping
-            if (a == 1)
-                a = 0;
+            if ((i + j) % 2 == 0)
+                cout << 1;
             else
-                a = 1;
+                cout << 0;
         }
         cout << endl;
     }
