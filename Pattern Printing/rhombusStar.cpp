05e7ad7176a0770ee0430577f060757920ec6753
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-08-26 23:39:40 +0530

    added on patern printing

---
+++ Pattern Printing/rhombusStar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the no. of rows : ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        // for spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        // for stars
+        for (int k = 1; k <= n; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
